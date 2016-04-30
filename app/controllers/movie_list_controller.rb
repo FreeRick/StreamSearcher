5d8aca0e23857fc75b121371fbb0725dbd4296a8
Daniel <daniel@gmail.com>
2016-04-30 16:49:53 -0400

    Fixing erb errors

--- app/controllers/movie_list_controller.rb
+++ app/controllers/movie_list_controller.rb
@@ -4,11 +4,9 @@ class MovieListController < ApplicationController
   end
 
   def searchAPI
-
     userinput = params[:userinput]
 
 
-      guidebox_request = HTTParty.get('http://api-public.guidebox.com/v1.43/US/rKzzNhxn0Pw7LcthbwDKx1SiZIIYwa0t/search/movie/title/'+userinput+')
-
+      guidebox_request = HTTParty.get("http://api-public.guidebox.com/v1.43/US/rKzzNhxn0Pw7LcthbwDKx1SiZIIYwa0t/search/movie/title/'+userinput+'")
   end
 end
