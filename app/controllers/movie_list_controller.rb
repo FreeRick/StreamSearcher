13f857438027cb72a5400c4935b80f712d043c86
Daniel <daniel@gmail.com>
2016-04-30 16:19:13 -0400

    Working with API in index.html

--- app/controllers/movie_list_controller.rb
+++ app/controllers/movie_list_controller.rb
@@ -1,5 +1,14 @@
 class MovieListController < ApplicationController
   def index
-    @all = HTTParty.get("http://api-public.guidebox.com/v1.43/US/rKzzNhxn0Pw7LcthbwDKx1SiZIIYwa0t/search/movie/title/nightcrawler").parsed_response
+    # @all = HTTParty.get("http://api-public.guidebox.com/v1.43/US/rKzzNhxn0Pw7LcthbwDKx1SiZIIYwa0t/search/movie/title/nightcrawler").parsed_response
+  end
+
+  def searchAPI
+
+    userinput = params[:userinput]
+
+
+      guidebox_request = HTTParty.get('http://api-public.guidebox.com/v1.43/US/rKzzNhxn0Pw7LcthbwDKx1SiZIIYwa0t/search/movie/title/'+userinput+')
+
   end
 end
