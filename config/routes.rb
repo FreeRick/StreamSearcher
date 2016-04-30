1d062a33c93ddfa07b69b7223fd4fd8053ed5ace
juanfperez <juanfperez@me.com>
2016-04-30 16:53:12 -0400

    merging 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'movie_list/index'
+  get 'movie_list/index', to: 'movie_list#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
