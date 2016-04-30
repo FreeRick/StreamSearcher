4429341f5bb853ede3821433dfc8fc764adef817
Daniel <daniel@gmail.com>
2016-04-30 14:18:59 -0400

    Adding controllers that will pull up Nightcrawler

--- app/controllers/movie_list_controller.rb
+++ app/controllers/movie_list_controller.rb
@@ -1,4 +1,5 @@
 class MovieListController < ApplicationController
-  def Index
+  def index
+    @all = HTTParty.get("http://api-public.guidebox.com/v1.43/US/rKzzNhxn0Pw7LcthbwDKx1SiZIIYwa0t/search/movie/title/nightcrawler").parsed_response
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'movie_list/Index'
+  get 'movie_list/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
