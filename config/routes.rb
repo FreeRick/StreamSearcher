cb2d05157dfb1aeeee995fee9d56e2814a9f89c2
Daniel <daniel@gmail.com>
2016-05-01 13:54:04 -0400

    We can now index through users with the route users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post 'movie_lists/index', to: 'movie_list#searchAPI'
 
   get 'search', to: 'movie_list#searchAPI'
+
+  get 'users'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
