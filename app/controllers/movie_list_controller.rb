b97e8bf88305f4af2ebcce481a9d75cf4f17e6c5
Daniel <daniel@gmail.com>
2016-05-02 19:55:21 -0400

    It works, the damn thing works

--- app/controllers/movie_list_controller.rb
+++ app/controllers/movie_list_controller.rb
@@ -10,6 +10,7 @@ class MovieListController < ApplicationController
     guidebox_request = HTTParty.get("http://api-public.guidebox.com/v1.43/US/rKzzNhxn0Pw7LcthbwDKx1SiZIIYwa0t/search/movie/title/#{userinput}")
     @movie_info = guidebox_request["results"].first
     # @trailer = guidebox_request["results"]["trailers"]
+    # p @movie_info
 
   end
 
